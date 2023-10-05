68fb0c3fabf36c05b57cde5a0bc5159cda87be1e
MariaFjodorowa <laks.salmon@gmail.com>
2023-10-05 19:09:34 +0200

    tsv with forms and gitignore

--- wugs/word_forms/parse_ru_wiktionary.py
+++ wugs/word_forms/parse_ru_wiktionary.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import os
 import time
 import unicodedata
@@ -6,28 +5,29 @@ from pathlib import Path
 import re
 
 from bs4 import BeautifulSoup
+import pandas as pd
 from requests import get
 
-
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
 
 BAD = ("у̀", "е́", "и́", "о́", "у́", "а́", "я́", "ы́", "à", "э́", "ю́")
 GOOD = ("у", "е", "и", "о", "у", "а", "я", "ы", "а", "э", "ю")
 
+
 def strip_accents(s):
-   norm = unicodedata.normalize('NFC', s)
-   for bad, good in zip(BAD, GOOD):
-       norm = norm.replace(bad, good)
-   return norm
+    norm = unicodedata.normalize('NFC', s)
+    for bad, good in zip(BAD, GOOD):
+        norm = norm.replace(bad, good)
+    return norm
+
 
 def read_html_wiktionary(word, results):
     source = f"https://ru.wiktionary.org/wiki/{word}"
     response = get(source, headers=HEADERS)
     contents = response.text
-    # with open(outfile, "w", encoding="utf8") as fout:
-    #     fout.write(contents)
+
     soup = BeautifulSoup(contents, "lxml")
-    #print(soup.prettify())
     word_results = []
     tbody = soup.find(title="падеж").parent.parent.parent
     for td_tag in tbody.find_all("td", bgcolor="#ffffff"):
@@ -36,6 +36,8 @@ def read_html_wiktionary(word, results):
         for text in td_tag_texts:
             if text.isalpha():
                 word_results.append(text)
+                if "ё" in text:
+                    word_results.append(text.replace("ё", "е"))
     text_string = " ".join(word_results) + "\n"
     print(text_string)
     results.append(text_string)
@@ -45,17 +47,17 @@ def read_html_wiktionary(word, results):
 if __name__ == "__main__":
     results = []
     words = set()
-
+    words_list = []
     print(strip_accents("авторите́т"))
     print(strip_accents("мой"))
 
-
-    for p in Path('./').glob('rushifteval_public/*/*/data/*'):
+    for p in Path('../').glob('rushifteval_public/*/*/data/*'):
         word = os.path.split(p)[-1]
         words.add(word)
     for word in words:
+        words_list.append(word)
         results = read_html_wiktionary(word, results)
 
         time.sleep(3)
-    with open("ru.txt", "w", encoding="utf8") as f:
-        f.writelines(results)
+    df = pd.DataFrame({"words": words_list, "forms": results})
+    df.to_csv("ru_forms.tsv", sep="\t", index=False)
