37befc5f270ebf586a18ea47f0892c818f60ad69
nvanva <nick.arefyev@gmail.com>
2023-10-17 18:34:16 +0200

    GlossReader's predictions for dwug_en, dwug_de: selecting glosses from English WordNet; XLMR-large with dot product gloss-use similarity, other hyperparameters selected by P(diff_gloss|1)

---
+++ predictions/glossreader_v1/select_hypers.sh
@@ -0,0 +1,2 @@
+cp -r ../../glmlarge_wordnet_glosses/clusterglosses-topk_10-by_count-predction_gloss/glosses_uses_dot/dwug_de ./
+cp -r ../../glmlarge_wordnet_glosses/clusterglosses-topk_3-by_count-predction_gloss/glosses_uses_dot/dwug_en ./
