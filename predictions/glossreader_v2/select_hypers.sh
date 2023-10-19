f9523d8e70241cd12f7816243aad6c8eac59aba4
nvanva <nick.arefyev@gmail.com>
2023-10-19 22:29:47 +0200

    GlossReader's predictions for RuDSI fixed, removed incorrectly selected glossreader_v2

--- predictions/glossreader_v2/select_hypers.sh
+++
@@ -1,2 +0,0 @@
-cp -r ../../glmbase_wordnet_glosses/clusterglosses-topk_1-by_count-predction_gloss/glosses_uses_l1/dwug_de ./
-cp -r ../../glmbase_wordnet_glosses/clusterglosses-topk_3-by_similarity-predction_gloss/glosses_uses_l1/dwug_en ./
