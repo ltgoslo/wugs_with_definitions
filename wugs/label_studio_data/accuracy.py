bb93e63101a174b2c25eae180c993cebd48b8b15
MariaFjodorowa <laks.salmon@gmail.com>
2023-10-20 18:36:13 +0200

    accuracy

--- wugs/label_studio_data/accuracy.py
+++ wugs/label_studio_data/accuracy.py
@@ -1,10 +1,11 @@
 import argparse
-from collections import defaultdict
+from collections import Counter
 import json
 import logging
 import os
 
 import pandas as pd
+from sklearn.metrics import accuracy_score
 
 logging.basicConfig(level=logging.INFO)
 METHODS = {
@@ -68,69 +69,63 @@ def main():
     annotations_path = os.path.expanduser(args.input_path)
     with open(annotations_path, "r", encoding="utf8") as f:
         annotations = json.load(f)
-    correct_answers = defaultdict(list)
-    both = defaultdict(int)
-    none = defaultdict(int)
     lang = args.lang.split("_")[0]
     gloss_repo = args.gloss_repo
     predictions_folder = os.path.join(gloss_repo, "predictions")
+    annotations_dict = {}
     for sample in annotations:
         # TODO don't uppercase my_text
-        word = sample["data"]["my_text"].split(": ")[0].lower()
-        gloss = sample["data"]["my_text"][len(word) + 2:].replace("<b>",
-                                                                  "").replace(
-            "</b>", "")
-        methods_with_this_gloss, clusters_pred = [], []
-        for method in METHODS[args.lang]:
+        word_gloss = sample["data"]["my_text"]
+        cluster_1 = sample["data"]["variants"][0]["value"]
+        cluster_2 = sample["data"]["variants"][1]["value"]
+
+        sample_annotations = sample['annotations']
+        for annotation in sample_annotations:
+            cluster_true = annotation['result'][0]["value"]["choices"][0]
+            annotations_dict[(word_gloss, cluster_1, cluster_2)] = cluster_true
+
+    for method in METHODS[args.lang]:
+        y_true, y_pred = [], []
+        for word in os.listdir(os.path.join(predictions_folder,
+                                        f"{method}/dwug_{lang}")):
             predictions_path = os.path.join(predictions_folder,
-                                            f"{method}/dwug_{lang}/{word}/cluster_gloss.tsv")
-            if not os.path.exists(predictions_path):
-                word = word[0].upper() + word[1:]
-                word = WORDS_MAPPING.get(word, word)
-                predictions_path = os.path.join(predictions_folder,
-                                                f"{method}/dwug_{lang}/{word}/cluster_gloss.tsv")
+                                        f"{method}/dwug_{lang}/{word}/cluster_gloss.tsv")
+
+            mapping_path = os.path.join(gloss_repo, f"wugs/label_studio_data/mappings/{lang}/{word}.tsv")
+            try:
+                mapping = pd.read_csv(mapping_path, sep="\t").astype(str)
+            except FileNotFoundError:  # no annotation for this word e.g. if all its clusters are -1 etc.
+                continue
             clusters_and_definitions = pd.read_csv(predictions_path, sep="\t")
             clusters_and_definitions[CLUSTER_NUMBER_COLUMN] = \
                 clusters_and_definitions[CLUSTER_NUMBER_COLUMN].astype(str)
             clusters_and_definitions = clusters_and_definitions[
                 clusters_and_definitions[CLUSTER_NUMBER_COLUMN] != "-1"]
 
-            cluster_pred = clusters_and_definitions[
-                clusters_and_definitions.gloss.str.lower() == gloss.lower()
-                ]
-            if cluster_pred.shape[0] > 0:
-                methods_with_this_gloss.append(method)
-                clusters_pred.append(
-                    cluster_pred[
-                        CLUSTER_NUMBER_COLUMN].unique().tolist())  # TODO assert without unique
-
-        sample_annotations = sample['annotations']
-        for annotation in sample_annotations:
-            cluster_true = annotation['result'][0]["value"]["choices"][0]
-            for method, clusters in zip(methods_with_this_gloss,
-                                        clusters_pred):
-                logging.info(method)
-                logging.info(cluster_true)
-                logging.info(clusters)
-                number_of_clusters_predicted = len(clusters)
-                correct_answers[method].append(
-                    int(cluster_true in clusters) / number_of_clusters_predicted,
-                )
-                if cluster_true == "-3":
-                    both[method] += 1
-                elif cluster_true == "-2":
-                    none[method] += 1
-
-    for method, v in correct_answers.items():
-        number_of_annotations = len(v)
-        accuracy = sum(v) / number_of_annotations
+            for row in clusters_and_definitions.iterrows():
+                cluster_1, definition = row[1][CLUSTER_NUMBER_COLUMN], row[1]["gloss"]
+                word_gloss = f"{word.upper()}: <b>{definition.upper()}</b>"
+                cluster_2 = mapping[mapping[CLUSTER_NUMBER_COLUMN] == cluster_1]["wrong_cluster"]
+                if cluster_2.shape[0]:  # maybe cluster with < 3 usages
+                    cluster_2 = cluster_2.iloc[0]
+                else:
+                    continue
+                cluster_true = annotations_dict.get((word_gloss, cluster_1, cluster_2))
+                if cluster_true is None:
+                    cluster_true = annotations_dict.get(
+                        (word_gloss, cluster_2, cluster_1))
+                y_true.append(cluster_true)
+                y_pred.append(cluster_1)
+        accuracy = accuracy_score(y_true, y_pred)
+        logging.info(f"{len(y_true)} clusters")
         logging.info(
-            f"Accuracy {method}: {round(accuracy * 100, 2)} on {number_of_annotations} annotations",
+            f"Accuracy {method}: {round(accuracy * 100, 2)}",
         )
-        none_metric = round((none[method] / number_of_annotations) * 100, 2)
+        answers_counter = Counter(y_true)
+        none_metric = round((answers_counter["-2"] / len(y_true)) * 100, 2)
         logging.info(
             f"{method}, definitions that describe none of the clusters {none_metric}")
-        both_metric = round((both[method] / number_of_annotations) * 100, 2)
+        both_metric = round((answers_counter["-3"] / len(y_true)) * 100, 2)
         logging.info(
             f"{method}, definitions that describe both clusters {both_metric}")
 
